f14d3cc9aaae0b3af8bfae52c80fa142319d3de4
Leonid <leeonidix@gmail.com>
2022-07-14 02:02:59 +0500

    Task 003 Done

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AskNum(int a)
+{
+    int res;
+    if (a > 0)
+    {
+        Console.Write($"Enter {a} number: ");
+        res = int.Parse(Console.ReadLine());
+        return res;
+    }
+    else
+    {
+        Console.Write($"Enter number: ");
+        res = int.Parse(Console.ReadLine());
+        return res;
+    }
+
+}
+
+int day = AskNum(0);
+if (day == 6 || day == 7)
+{
+    Console.WriteLine($"{day} -> Yes");
+}
+else
+{
+    Console.WriteLine($"{day} -> No");
+}
