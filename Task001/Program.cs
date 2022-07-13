8dfb3a1f031b4519def380fca83bb517bd257c34
Leonid <leeonidix@gmail.com>
2022-07-13 22:50:08 +0500

    Ttask 001 Done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+int SecNum(int a)
+{
+    int secondNum = a / 10 % 10;
+    return secondNum;
+}
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"{num} -> {SecNum(num)}");
